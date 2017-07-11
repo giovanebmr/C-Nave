025bfcc095fd539ec3be8d289698a7b83131e84f
giovane <sonata14>
2017-07-10 21:57:12 -0300

    iniciando o projeto: definição das bibliotecas utilizadas, matriz e loop principal

---
+++ main/C-Nave 0.0.1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>   //para printf
+#include <stdlib.h>  //para system pause cls e etc
+#include <conio.h>   //para leitura do teclado   kbhit() e getch()
+#include <unistd.h>	 //para sleep (segundos) e usleep (microsegundos)
+#include <windows.h> //para sons
+
+//Configurações iniciais
+#define TAMANHO_X 20 //largura utilizável da tela
+#define TAMANHO_Y 20 //altura utilizável da tela
+#const char ESPACO = '*'; //forma dos espaços na matriz
+
+
+int main(){
+	
+	char tela [TAMANHO_Y][TAMANHO_X];
+	
+	//Inicializando a matriz com o caracter definido
+	for( int y = 0 ; y < TAMANHO_Y ; y++ ){
+		for( int x = 0 ; x < TAMANHO_X ; x++ ){
+			tela[y][x] = ESPACO;
+		}	
+	}
+	
+	while( true ){
+		//Imprime a matriz na tela
+		for( int y = 0 ; y < TAMANHO_Y ; y++ ){
+			for( int x = 0 ; x < TAMANHO_X ; x++ ){
+				tela[y][x] = ESPACO;
+			}	
+		}
+	}
+	
+	
+	
+	printf("teste");
+	
+	return 0;
+}
